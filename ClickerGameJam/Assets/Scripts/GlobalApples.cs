3671582856ef39ab540ce1161bb1c474b3fa5429
Yiannis04 <91196016+Yiannis04@users.noreply.github.com>
2022-09-03 11:17:23 -0400

    Added Buttons and Simple Mechanics

---
+++ ClickerGameJam/Assets/Scripts/GlobalApples.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalApples : MonoBehaviour
+{
+    public static int AppleCount; 
+    public GameObject AppleDisplay; 
+    public int InternalApple;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InternalApple = AppleCount;
+        AppleDisplay.GetComponent<Text>().text = "Apples: " + InternalApple;
+    }
+}
---
+++ ClickerGameJam/Assets/Scripts/SellApple.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellApple : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public void ClickTheButton ()
+    {
+        GlobalApples.AppleCount -= 1;
+        GlobalCash.CashCount += 1;
+    }
+
+
+}
