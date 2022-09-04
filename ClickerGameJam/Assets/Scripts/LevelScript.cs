d387812914289b273f05057a481c35b03619cdb8
Yiannis04 <91196016+Yiannis04@users.noreply.github.com>
2022-09-04 17:35:55 -0400

    Added Main Menu and Other Levels

---
+++ ClickerGameJam/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LevelScript : MonoBehaviour
+{
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+
+}
